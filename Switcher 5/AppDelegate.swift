239b254a1f810d6ef952cab414ffe20e962d4374
Michel Koga <koga.michel@gmail.com>
2018-07-31 15:37:10 +0900

    Initial Commit

---
+++ Switcher 5/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Switcher 5
+//
+//  Created by 古賀ミッシェル on 2018/07/31.
+//  Copyright © 2018 古賀ミッシェル. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Switcher 5/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Switcher 5
+//
+//  Created by 古賀ミッシェル on 2018/07/31.
+//  Copyright © 2018 古賀ミッシェル. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
