87091f035ab156e98b2221876ad563ee755e03ad
Michel Koga <koga.michel@gmail.com>
2018-08-02 21:42:12 +0900

    [ and - delete button corrected

--- Switcher 5/CustomizeViewController.swift
+++ Switcher 5/CustomizeViewController.swift
@@ -20,15 +20,7 @@ class CustomizeViewController: NSViewController {
 		UserDefaults.standard.set(true, forKey: "appChanged")
 		self.dismiss(self)
 	}
-//	fileprivate func drawButtons() {
-//		for case let button as CustomizeButton in self.view.subviews {
-//			if button.tag != 1 {
-//				let appName = button.appName
-//				button.image = NSImage(named: NSImage.Name(rawValue: appName))
-//			}
-//
-//		}
-//	}
+	
 	@IBAction func removeApp(_ sender: NSButton) {
 		let character = UserDefaults.standard.string(forKey: "chosenKey")
 		UserDefaults.standard.set("", forKey: character!)
